25d40ca97cde88b4ab83f0bac5eb26392d07e23a
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-03-11 10:16:47 -0400

    started skeleton

---
+++ Quick.java
@@ -0,0 +1,12 @@
+public class Quick{
+    /*return the value that is the kth smallest value of the array.
+ */
+ public static int quickselect(int []data, int k){
+
+ }
+
+ /*@return the index of the final position of the pivot element.
+ */
+ public static int partition ( int [] data, int start, int end){
+ }
+}
