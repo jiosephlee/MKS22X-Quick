033aff569c2c53f6e0ec6a643d9b29e4df64ddaa
John Dogit config --global user.name John <jlee01@stuy.edu>
2019-03-12 10:17:05 -0400

    end of period

--- Quick.java
+++ Quick.java
@@ -1,12 +1,34 @@
+import java.util.*;
 public class Quick{
     /*return the value that is the kth smallest value of the array.
  */
  public static int quickselect(int []data, int k){
-
  }
 
  /*@return the index of the final position of the pivot element.
  */
- public static int partition ( int [] data, int start, int end){
+ public static int partition (int [] data, int start, int end){
+     Random rand = new Random();
+     int index = rand.nextInt(data.length);
+     //pivot swap
+     int hold = data[start];
+     data[start] = data[index];
+     data[index] = hold;
+     int pivot = start;
+     start++;
+     //start comparing and swapping;
+     for(int i = start; i < data.length; i++){
+         if(data[i] > data[pivot]){
+             hold = data[i];
+             data[i] = data[end];
+             data[end] = hold;
+             i--;
+         } else if(data[i] < data[pivot]){
+             hold = data[i];
+             data[i] = data[pivot];
+             data[pivot] = hold;
+             pivot = i;
+         }
+     }
  }
 }
