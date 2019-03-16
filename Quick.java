1f4aac54a28bbd06b8781c505ffac3d78be4ba86
Joseph Lee <jlee01@stuy.edu>
2019-03-16 15:07:19 -0400

    tested using mr.K's driver, woah pretty quick

--- Quick.java
+++ Quick.java
@@ -17,7 +17,40 @@ public class Quick{
      System.out.println();
      test = new int[] {1,0,5,7,9,8,3,6};
      System.out.println(quickselect(test,4));
- }
+//added mrks driver
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}
  public static int quickselect(int []data, int k){
      int min = 0;
      int max = data.length - 1;
@@ -112,6 +145,9 @@ public class Quick{
 }
  /*Modify the array to be in increasing order.
  */
+ public static void quicksort(int[] data){
+     quicksort(data,0,data.length-1);
+ }
  public static void quicksort(int[] data, int lo, int hi){
      if (lo >= hi){
          return;
